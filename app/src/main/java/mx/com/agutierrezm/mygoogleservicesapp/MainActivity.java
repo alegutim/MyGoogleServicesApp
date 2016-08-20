07ad8bd142baba0fd76b73004147704dd1415dd6
alegutim <sonic_a_18@homtail.com>
2016-08-19 20:21:58 -0500

    segundo commit

--- app/src/main/java/mx/com/agutierrezm/mygoogleservicesapp/MainActivity.java
+++ app/src/main/java/mx/com/agutierrezm/mygoogleservicesapp/MainActivity.java
@@ -1,11 +1,9 @@
 package mx.com.agutierrezm.mygoogleservicesapp;
 
 import android.content.Intent;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
